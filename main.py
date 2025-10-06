3bc08fb83c3a43058793e0163ed0f7db77e689d8
chandra075 <34832899+chandra075@users.noreply.github.com>
2025-10-06 20:40:31 +0530

    Update main.py
    Line 1 modified by another team member

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+# This is a sample Python script modified by another team member on 6th October post line 2 change
 # This is a new line added on 3rd October 2025 but changed on 6th October 2025.
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
